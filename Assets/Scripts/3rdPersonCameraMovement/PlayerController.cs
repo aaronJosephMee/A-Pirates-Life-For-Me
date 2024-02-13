9aa2c796086d0f28931f7fdd12e548f26054c687
Keldon Gamble <kfg324@usask.ca>
2024-02-13 15:05:37 -0600

    Mini game prototype

---
+++ Assets/Scripts/3rdPersonCameraMovement/PlayerController.cs
@@ -0,0 +1,92 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
+
+public class PlayerController : MonoBehaviour
+{
+    private const float Gravity = -9.81f;
+    private const float RotationSpeed = 500f;
+    private const float JumpPower = 2.0f;
+    private float _verticalVelocity;
+    private Vector2 _input;
+    private Vector3 _direction;
+    private CharacterController _characterController;
+    private Animator _playerModelAnimator;
+    [SerializeField] private GameObject playerModel;
+    [SerializeField] private GameObject cameraPosition;
+    [SerializeField] private float speed;
+    private static readonly int RunningAnimationFlag = Animator.StringToHash("Running");
+
+    private void Awake()
+    {
+        _playerModelAnimator = playerModel.GetComponent<Animator>();
+        _characterController = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        SetAnimationFlags();
+        ApplyRotation();
+        ApplyGravity();
+        ApplyMovement();
+    }
+
+    private void SetAnimationFlags()
+    {
+        _playerModelAnimator.SetBool(RunningAnimationFlag, _input.sqrMagnitude != 0);
+    }
+
+    private void ApplyRotation()
+    {
+        if (_input.sqrMagnitude == 0)
+        {
+            return;
+        }
+        _direction = Quaternion.Euler(0.0f, cameraPosition.transform.eulerAngles.y, 0.0f) * new Vector3(_input.x, 0.0f, _input.y);
+        Quaternion targetRotation = Quaternion.LookRotation(_direction, Vector3.up);
+        playerModel.transform.rotation = Quaternion.RotateTowards(playerModel.transform.rotation, targetRotation,
+            RotationSpeed * Time.deltaTime);
+    }
+    
+    private void ApplyGravity()
+    {
+        if (_characterController.isGrounded && _direction.y < 0.0f)
+        {
+            _verticalVelocity = -1.0f;
+        }
+        else
+        {
+            _verticalVelocity += Gravity * Time.deltaTime;
+        }
+
+        _direction.y = _verticalVelocity;
+    }
+    
+    private void ApplyMovement()
+    {
+        _characterController.Move(_direction * (speed * Time.deltaTime));
+    }
+    
+    public void GetMovementInput(InputAction.CallbackContext context)
+    {
+        _input = context.ReadValue<Vector2>();
+        _direction = new Vector3(_input.x, 0.0f, _input.y);
+    }
+
+    public void GetJumpInput(InputAction.CallbackContext context)
+    {
+        if (!context.started)
+        {
+            return;
+        }
+
+        if (_characterController.isGrounded)
+        {
+            _verticalVelocity = JumpPower;
+            _direction.y = _verticalVelocity;
+        }
+    }
+}
---
+++ Assets/Scripts/wood minigame/woodminigame.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class WoodMiniGame : MonoBehaviour
+{
+    public Canvas image;
+    public int treeHP = 6;
+
+    private int chops;
+    private float SwingTime;
+
+    private bool input;
+
+    void Start()
+    {
+        image.enabled = false;
+        input = false;
+        chops = 0;
+        SwingTime = Time.time + Random.Range(1.5f, 3.0f);
+    }
+
+    void Update()
+    {
+        if (Time.time >= SwingTime)
+        {
+            DisplayKey();
+            SwingTime = Time.time + Random.Range(1.5f, 3.0f);
+        }
+
+        if (input)
+        {
+            CheckInput();  
+        }
+    }
+
+    void DisplayKey()
+    {
+        image.enabled = true;
+        input = true;
+        StartCoroutine(HideKey());
+    }
+
+    void CheckInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            chops++;
+            Debug.Log("Chop " + chops + "/" + treeHP);
+            if (chops >= treeHP)
+            {
+                Debug.Log("tree defeated");
+                chops = 0;
+                Destroy(gameObject);
+            }
+            input = false;
+        }
+    }
+
+    IEnumerator HideKey()
+    {
+        yield return new WaitForSeconds(0.5f);
+        image.enabled = false;
+        input = false;
+    }
+}
