42f29abfe6282f9d4044f7b02d149f2e6e238b4f
JL M <jldmeshow@gmail.com>
2024-02-13 19:46:57 -0600

    Add fast travel between island scenes and create minigame interactable

---
+++ Assets/Scripts/MinigameInteractable.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class MinigameInteractable : MonoBehaviour, IInteractable
+    {
+        public string scene;
+        public void Interact(Collider collider)
+        {
+            GameManager.instance.LoadScene(scene, false);
+        }
+    }
+}
