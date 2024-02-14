238599810df61c57d7542032f65d4af6419ef8e6
Connor <lrk312@usask.ca>
2024-02-14 00:02:40 -0600

    Added a thing to show flags work and track between scenes

--- Assets/Scripts/Choice/FlagValue.cs
+++ Assets/Scripts/Choice/FlagValue.cs
@@ -9,6 +9,7 @@ public class FlagValue : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        this.gameObject.SetActive(false);
         GameManager.choices.CreateDependency("Wood", this.gameObject, 1, Activate);
     }
 
