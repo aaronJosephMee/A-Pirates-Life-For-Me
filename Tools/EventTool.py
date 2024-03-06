08ec2c7a748bea0782878448c9f59a9e80e4e9c2
Connor <lrk312@usask.ca>
2024-03-06 17:15:44 -0600

    Updated event tool

--- Tools/EventTool.py
+++ Tools/EventTool.py
@@ -15,24 +15,31 @@ if minigame == "y":
     nameOfGame = input("Title of minigame to be displayed: ")
     print(f"{title}\n{name}\n{flavor}\n{scene}\n{camera}\nTrue\n{nameOfGame}")
     exit()
-
 numbChoice = int(input("How many choices will the event have?: "))
 choices = []
 for i in range (0, numbChoice):
-    Ctext = input("Text of the choice: ")
+    Ctext = input("Text of choice " + str(i + 1) + ": ")
     Ceffects = ""
     for stat in stats:
-        Ceffect = input("How much " + stat + " will the event give?: ")
+        Ceffect = input("How much " + stat + " will the event give? (blank input == 0): ")
+        if Ceffect == "":
+            Ceffect = "0"
         if stat != stats[0]:
             Ceffects += "\n" 
         Ceffects += stat + " " + Ceffect
-    numbItems = int(input("How many items will this choice give?: "))
+    numbItems = input("How many items will this choice give? (blank input == 0): ")
+    if numbItems == "":
+            numbItems = "0"
+    numbItems = int(numbItems)
     Citems = ""
     for y in range(0, numbItems):
         Citems += input("Title of item to be given: ")
         if (y < numbItems - 1):
             Citems += "\n"
-    numbEvents = int(input("How many events will this choice add to the pool: "))
+    numbEvents = input("How many events will this choice add to the pool (blank input == 0): ")
+    if numbEvents == "":
+            numbEvents = "0"
+    numbEvents = int(numbEvents)
     Cevents = ""
     for y in range(0, numbEvents):
         Cevents += input("Title of event to add: ")
