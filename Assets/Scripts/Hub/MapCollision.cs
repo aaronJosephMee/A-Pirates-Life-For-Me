88323f0ac941689d58a3a8e3cf18e8d6cd832abb
Nolan Aung <nolanaung@lm-b0be833e71c6.dhcp.usask.ca>
2024-03-12 14:45:37 -0600

    Added steering wheel collision for the map.

---
+++ Assets/Scripts/Hub/MapCollision.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapCollision : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Insert Map interaction");
+        }
+    }
+
+}
