f6e09e0c85b47d621b63c2e486feba5356abe613
Connor <lrk312@usask.ca>
2024-03-11 19:18:16 -0600

    Made a data structure to store player relics and stuff

---
+++ Assets/Scripts/Items/PlayerItems.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerItems
+{
+    Dictionary<string, Item> playerRelics;
+    Dictionary<string, Item> playerWeapons;
+    Item currentItem;
+    Item currentWeapon;
+
+    public Item GetItem(){
+        return currentItem;
+    }
+    public void SetItem(Item item){
+        currentItem = item;
+    }
+    public Item GetWeapon(){
+        return currentWeapon;
+    }
+    public void SetWeapon(string weapon){
+        currentWeapon = playerWeapons[weapon];
+    }
+    public Dictionary<string, Item> GetRelics(){
+        return playerRelics;
+    }
+    public void AddRelic(KeyValuePair<string, Item> relic){
+        playerRelics.Add(relic.Key, relic.Value);
+    }
+    public Dictionary<string, Item> GetWeapons(){
+        return playerWeapons;
+    }
+    public void AddWeapon(KeyValuePair<string, Item> weapon){
+        playerWeapons.Add(weapon.Key, weapon.Value);
+    }
+
+}
