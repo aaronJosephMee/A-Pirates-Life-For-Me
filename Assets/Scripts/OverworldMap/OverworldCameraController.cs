9e2222784afe802ed370a53f250b8abe9f1912ff
JL M <jldmeshow@gmail.com>
2024-03-30 01:16:06 -0600

    Add visuals
    - Add wind effect and fog to overworld map
    - Make small tweaks to combat scene

--- Assets/Scripts/OverworldMap/OverworldCameraController.cs
+++ Assets/Scripts/OverworldMap/OverworldCameraController.cs
@@ -8,7 +8,11 @@ public class OverworldCameraController : MonoBehaviour
 {
     private Vector2 _input = Vector2.zero;
 
-    private float _cameraSpeed = 1000.0f;
+    [SerializeField] private float cameraSpeed;
+
+    [SerializeField] private float minPosition;
+
+    [SerializeField] private float maxPosition;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,15 +28,17 @@ public class OverworldCameraController : MonoBehaviour
     public void GetMovementInput(InputAction.CallbackContext context)
     {
         _input = context.ReadValue<Vector2>();
+        print(gameObject.name);
     }
 
     private void LateUpdate()
     {
+        print(gameObject.name);
         if (_input.x != 0)
         {
             Vector3 newPosition = transform.position;
-            newPosition.x += _input.x * _cameraSpeed * Time.deltaTime;
-            newPosition.x = Mathf.Clamp(newPosition.x, -250.0f, 250.0f);
+            newPosition.x += _input.x * cameraSpeed * Time.deltaTime;
+            newPosition.x = Mathf.Clamp(newPosition.x, minPosition, maxPosition);
             transform.position = newPosition;
         }
     }
