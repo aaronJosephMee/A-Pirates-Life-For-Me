1d7e1dd6c092e557a39d9f5a01c1258b1fbb884a
Nolan Aung <nolanaung@Nolans-Air.gateway.sasktel.net>
2024-04-01 18:26:07 -0600

    UI frames are visually done, code needs to be implemeted/fixed

--- Assets/Scripts/Hub/shipHubTut.cs
+++ Assets/Scripts/Hub/shipHubTut.cs
@@ -1,18 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
+using DefaultNamespace.OverworldMap;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Assertions.Must;
+using UnityEngine.UI;
 
 public class shipHubTut : MonoBehaviour
 {
     // Start is called before the first frame update
+    public GameObject page1IMG;
+    public GameObject page1Text;
+    public GameObject page2IMG;
+    public GameObject page2Text;
+
     void Start()
     {
         
+        page1IMG.gameObject.SetActive(true);
+        page1Text.gameObject.SetActive(true);
+        page2IMG.gameObject.SetActive(false);
+        page2Text.gameObject.SetActive(false);
+
+
+        
+        Cursor.visible = true;
+        GetComponent<Button>().onClick.AddListener(next);
     }
 
     // Update is called once per frame
-    void Update()
-    {
+        void Update()
+        {
+
+        }
+
+        void next()
+        {
+            page1IMG.gameObject.SetActive(false);
+            page1Text.gameObject.SetActive(false);
+            page2IMG.gameObject.SetActive(true);
+            page2Text.gameObject.SetActive(true);
+            this.GetComponent<Button>().onClick.AddListener(delegate{Destroy(this.transform.parent.gameObject);});
+        }
         
-    }
+
 }
+
