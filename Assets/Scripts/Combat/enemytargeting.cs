c86d90b88aa7d22c05adf10063880b29ed3050c4
amacadaeg <aaronmacadaeg@gmail.com>
2024-04-04 03:36:05 -0600

    bossfight
    hi

--- Assets/Scripts/Combat/enemytargeting.cs
+++ Assets/Scripts/Combat/enemytargeting.cs
@@ -26,6 +26,8 @@ public class enemytargeting : MonoBehaviour
 
     public bool isEnemy = false;
 
+    public bool isEnemy = false;
+
 
     // Start is called before the first frame update
     void Start()
