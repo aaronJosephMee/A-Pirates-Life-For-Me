5bac5f20628c6d9120c2f6b40d89746140059904
JL M <jldmeshow@gmail.com>
2024-04-04 04:18:21 -0600

    fix merge

--- Assets/Scripts/Combat/enemytargeting.cs
+++ Assets/Scripts/Combat/enemytargeting.cs
@@ -26,8 +26,6 @@ public class enemytargeting : MonoBehaviour
 
     public bool isEnemy = false;
 
-    public bool isEnemy = false;
-
 
     // Start is called before the first frame update
     void Start()
